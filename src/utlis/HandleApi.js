9f85be176dee095b832040fff3eb909974a73007
aiyus <aiyusboss@gmail.com>
2023-06-19 08:22:50 +0100

    more

--- src/utlis/HandleApi.js
+++ src/utlis/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://todo-backend-a6nk.onrender.com";
 
 const getAllTodo = (setTodo) => {
   axios.get(baseUrl).then(({ data }) => {
